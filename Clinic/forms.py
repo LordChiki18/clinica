bdd6322721bd15074fd6724137b996bb6e105be5
LordChiki18 <cristiandomecqc@gmail.com>
2023-08-26 17:37:44 -0400

    AC

--- Clinic/forms.py
+++ Clinic/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from .models import Paciente, Rac
+from .models import Paciente
 
-class user_form(forms.ModelForm):
+class userForm(forms.ModelForm):
     class Meta:
         model = Paciente
-        fields = ['']
+        fields = ['usuario', 'password', 'nombre', 'apellido']
+        labels = {
+            'usuario': '',  # Puedes establecer la etiqueta que desees aquí
+            'password': '',  # Puedes establecer la etiqueta que desees aquí
+            'nombre': '',  # Puedes establecer la etiqueta que desees aquí
+            'apellido': '',  # Puedes establecer la etiqueta que desees aquí
+        }
+        widgets = {
+            'password': forms.PasswordInput(),  # Por ejemplo, para ocultar el campo de contraseña
+        }
