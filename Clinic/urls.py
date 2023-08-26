e8bb55654c5b964a27c10a446af773f3fe994f70
LordChiki18 <cristiandomecqc@gmail.com>
2023-08-26 18:22:55 -0400

    Changes by hucap

--- Clinic/urls.py
+++ Clinic/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'Clinic'
 urlpatterns = [
     #Home Page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('registro/', views.registro_paciente, name="registro_paciente"),
 ]
